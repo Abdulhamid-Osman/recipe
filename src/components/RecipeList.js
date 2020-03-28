1e1f2691115443b8268733cde00a2a9fff7b9197
jeffrey-dozva <jeffreydozva@gmail.com>
2020-03-28 17:52:57 +0300

    Jeffrey's changes

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,70 +1,94 @@
-import React, {Component} from 'react';
-import HttpService from '../services/http-service'
+import React, { Component } from "react";
+import HttpService from "../services/http-service";
 
 const http = new HttpService();
-class RecipeList extends Component{
-      constructor(props){
+
+//make the Recipe component
+
+const Recipe = props => {
+  const {
+    title,
+ingredients,
+instructions,
+difficulty,
+time,
+  } = props;
+  return (
+    <div className="card">
+      <div className="card-block">
+        <h2 className="card-title">{title}</h2>
+        <h3 className="class-ingredients">{ingredients}</h3>
+        <h3 className="class-description">{instructions}</h3>
+        <h3 className="class-difficulty">{difficulty}</h3>
+        <h3 className="class-duration">{time}</h3>
+      </div>
+    </div>
+  );
+};
+class RecipeList extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      recipes:[]
-    }
-  // Binding functions
+      recipes: [
+        {
+          title: "Test",
+          ingredients: "Test",
+          instructions: "Test",
+          difficulty: "Test",
+          time: "Test"
+        }
+      ]
+    };
+    // Binding functions
     this.loadData = this.loadData.bind(this);
     this.recipeList = this.recipeList.bind(this);
-    this.loadData();
+    // this.loadData();
   }
 
   componentDidMount() {
     // this.loadData();
   }
-  loadData = ()=>{
+  loadData = () => {
     const self = this;
-    http.getRecipes().then(data =>{
-      self.setState({
-        recipes: data
-      })
-    }, err=>{
+    http.getRecipes().then(
+      data => {
+        self.setState({
+          recipes: data
+        });
+      },
+      err => {}
+    );
+  };
 
-    });
-  }
-  
-  recipeList = ()=>{
-    const list = this.state.recipes.map((recipe)=>
+  recipeList = () => {
+    const list = this.state.recipes.map(recipe => (
       <div className="col-sm-4" key={recipe._id}>
-        <RecipeList title={recipe.title} ingredients={recipe.ingredients} instructions={recipe.instructions} difficulty={recipe.difficulty} time={recipe.time}/>
+        <Recipe //this is the component that we constructed up there ( its a dumb component ) you can google this
+          title={recipe.title}
+          ingredients={recipe.ingredients}
+          instructions={recipe.instructions}
+          difficulty={recipe.difficulty}
+          time={recipe.time}
+        />
       </div>
-    )
-    return (list);
+    ));
+    return list;
+  };
+  render() {
+    return <div className="container">{this.recipeList()}</div>;
   }
-  render(){
-    return(
-      
-        <div className="container">
-                <div className="card">
-                    <div className="card-block">
-                        <h2 className="card-title">{this.props.title}</h2>
-                        <h3 className="class-ingredients">{this.props.ingredients}</h3>
-                        <h3 className="class-description">{this.props.instructions}</h3>
-                        <h3 className="class-difficulty">{this.props.difficulty}</h3>
-                        <h3 className="class-duration">{this.props.time}</h3>
-                    </div>
-                </div>
-            </div>
-      
-    )
-  }
-    // constructor(props){
-    //     super(props)
-    //     http.getRecipes();
-    // }
-    // render(){
-    //     return(
-    //         <div>
-    //             <h1>Anun</h1>
-    //         </div>
-    //     )
-    // }
-};
+  // constructor(props){
+  //     super(props)
+  //     http.getRecipes();
+  // }
+  // render(){
+  //     return(
+  //         <div>
+  //             <h1>Anun</h1>
+  //         </div>
+  //     )
+  // }
+}
 export default RecipeList;
 // class App extends Component{
 //   constructor(props){
@@ -91,7 +115,7 @@ export default RecipeList;
 
 //     });
 //   }
-  
+
 //   recipeList = ()=>{
 //     const list = this.state.recipes.map((recipe)=>
 //       <div className="col-sm-4" key={recipe._id}>
@@ -116,4 +140,3 @@ export default RecipeList;
 //     </div>
 //   );
 // }
-
